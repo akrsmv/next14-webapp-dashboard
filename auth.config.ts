0ea756b9fdf14df877039d1347547f289a14d6b9
Krasimir Atanasov <krasimir.atanasoff@gmail.com>
2024-03-19 14:50:16 +0200

    temp change

--- auth.config.ts
+++ auth.config.ts
@@ -8,12 +8,18 @@ export const authConfig = {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            if (isOnDashboard) {
-                if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
-            } else if (isLoggedIn) {
-                return Response.redirect(new URL('/dashboard', nextUrl));
-            }
+            
+            // -------------------
+            // bypass logging in functionality for now
+            // if (isOnDashboard) {
+            //     if (isLoggedIn) return true;
+            //     return false; // Redirect unauthenticated users to login page
+            // } else if (isLoggedIn) {
+            //     return Response.redirect(new URL('/dashboard', nextUrl));
+            // }
+            // return true;
+            // --------------------
+            if (!isOnDashboard) {return Response.redirect(new URL('/dashboard', nextUrl));}
             return true;
         },
     },
